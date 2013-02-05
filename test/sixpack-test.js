dd68ad1c43d5387b5cffd6dbd593d11d3ae8f16d
Mike Dirolf <mike@dirolf.com>
2013-02-05 15:47:18 -0500

    More tests and implementation for convert/simple_convert.

--- test/sixpack-test.js
+++ test/sixpack-test.js
@@ -10,4 +10,25 @@ describe("Sixpack", function () {
             done();
         });
     });
+
+    it("should auto generate a client_id", function (done) {
+        var sixpack = require('../index');
+        sixpack.simple_participate("show_bieber", ["trolled", "not-trolled"], function(err, alt) {
+            if (err) throw err;
+            expect(alt).to.match(/trolled/);
+            done();
+        });
+    });
+
+    it("should return ok for simple-convert", function (done) {
+        var sixpack = require('../index');
+        sixpack.simple_participate("show-bieber", ["trolled", "not-trolled"], "mike", function(err, alt) {
+            if (err) throw err;
+            sixpack.simple_convert("show-bieber", "mike", function(err, alt) {
+                if (err) throw err;
+                expect(alt).to.equal("ok");
+                done();
+            });
+        });
+    });
 });
