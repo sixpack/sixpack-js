6f91b23b909ad078db167331a47307ac4b8dd48a
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-08-10 01:11:39 -0300

    refactor: change import

--- examples/node/basic.js
+++ examples/node/basic.js
@@ -1,6 +1,8 @@
-var sixpack = require('../../');
+var sixpack = require('../../dist/sixpack-server');
 
-var session = new sixpack.Session();
+var session = new sixpack.Session({
+  base_url: "http://localhost:8000",
+});
 session.participate("test-exp", ["alt-one", "alt-two"], function (err, res) {
   if (err) return console.log(err);
   console.log(res);
