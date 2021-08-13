fc5f1511aae1c4cd750449490ca244564f7a5947
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-08-13 16:10:51 -0300

    refactor: add eslint checks

--- src/session.js
+++ src/session.js
@@ -83,6 +83,7 @@ export default class Session {
   requestUri = (endpoint, params) => {
     const queryString = [];
     const e = encodeURIComponent;
+    // eslint-disable-next-line no-restricted-syntax
     for (const key in params) {
       // eslint-disable-next-line no-prototype-builtins
       if (params.hasOwnProperty(key)) {
